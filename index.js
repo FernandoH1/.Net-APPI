aed7b4098a257c4624a5e6b039ef6574ca7c56e2
Lucas <lucasmilessi98@gmail.com>
2022-10-12 21:41:48 -0300

    Se agregaron unas lineas en index.js para que admita imagenes largas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const pastura = require('./router/pastura.route'); // Importar las rutas de los productos
 
@@ -6,8 +7,12 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
+// Express 3.0
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb' }));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
